84c471917ba841469bcf5a31bac0869acf72fd67
Toru Hatashi <co_toru-hatashi@c-fo.com>
2020-10-27 04:01:37 +0900

    p199

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <template>
-  <HelloWorld
+  <Calc
     :title="message"
     v-on:result-event="appAction"
   />
   <hr>
-  <button v-on:click="doAction">change title</button>
-  <p>{{ result }}</p>
+  <div>
+    <table v-html="log"></table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/calc.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Calc
+  },
+  props: {
+    result: Array
   },
   data: function() {
     return {
-      message: 'HELLO',
-      result: 'no event'
+      message: 'CALC',
+      result: [],
+    }
+  },
+  computed: {
+    log: function() {
+      var table = '<tr><th class="head">Expression</th><th class="head">Value</th></tr>'
+
+      for(var i in this.result) {
+        table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][1] + '</th></tr>'
+      }
+      return table
+    }
+  },
+  created: function(){
+    var items = localStorage.getItem('log')
+    var logs = JSON.parse(items)
+    if (logs != null) {
+      this.result = logs
     }
   },
   methods: {
-    doAction: function() {
-      const input = prompt("new tile:")
-      this.message = input
-    },
-    appAction: function(message) {
-      this.result = '(*** you send:"' + message + '". ***)'
+    appAction: function(exp, res) {
+      this.result.unshift([exp, res])
+      if (this.result.length > 10) {
+        this.result.pop()
+      }
+      var log = JSON.stringify(this.result)
+      localStorage.setItem('log', log)
     }
   }
 }
@@ -39,8 +61,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 5px;
+}
+
+tr td {
+  padding: 5px;
+  border: 1px solid gray;
+}
+
+tr th {
+  padding: 5px;
+  border: 1px solid gray;
+}
+
+tr th.head {
+  background-color: black;
+  color: white;
 }
 </style>
